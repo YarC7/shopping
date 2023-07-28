ff584f3ad245cc25948124136889898b7b2c4daa
Cankkun <nguyenduccanh177@gmail.com>
2023-07-28 10:29:19 +0700

    3000C:

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // Cấu hình CORS ở đây
 
